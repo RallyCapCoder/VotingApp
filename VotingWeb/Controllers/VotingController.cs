e99a828463e65565c01088e84271f76ceefdedbe
RallyCapCoder <rallycapcoding@gmail.com>
2017-07-02 08:53:48 -0400

    Started to Add Logging to Application

--- VotingWeb/Controllers/VotingController.cs
+++ VotingWeb/Controllers/VotingController.cs
@@ -6,23 +6,29 @@ using System.Web.Mvc;
 using VotingApp.DataManagement;
 using VotingApp.Models;
 using VotingWeb.Models;
+using NLog;
 
 namespace VotingWeb.Controllers
 {
     public class VotingController : Controller
     {
+        private static Logger log = LogManager.GetLogger("VoteLog");
         // GET: Voting
         public ActionResult Index()
         {
             //When you go to page we should create a new ballot and save it to db
             var _manager = new VotingManager();
             var viewModel = new VotingViewModel();
-
+            log.Info("Creating Ballot");
             var ballot = _manager.CreateBallot("National Election" + DateTime.Now);
             viewModel.BallotId = ballot.BallotId;
+            log.Info("Getting Presidential and Vice Presidential Canindates");
             viewModel.PresidentAndVicePres = _manager.GetRankedVoteItems();
+            log.Info("Getting Supreme Court Canindate");
             viewModel.SupremeCourt = _manager.GetSingleVoteItems().FirstOrDefault(x => x.Issue == null);
+            log.Info("Getting State Representative Canindates");
             viewModel.StateRep = _manager.GetMultiVoteItems();
+            log.Info("Getting Ballot Issues");
             viewModel.BallotIssue = _manager.GetSingleVoteItems().FirstOrDefault(x => x.Canidate == null);
 
             return View(viewModel);
@@ -31,10 +37,11 @@ namespace VotingWeb.Controllers
         [HttpPost]
         public ActionResult Vote(VotingViewModel viewModel)
         {
-
+            log.Info("User Has Voted!");
             var _manager = new VotingManager();
-
+            log.Info("Saving Results");
             var electionResults = new List<VoteResult>();
+
             foreach (var presidentAndVice in viewModel.PresidentAndVicePres)
             {
                 electionResults.Add(new VoteResult
@@ -74,7 +81,7 @@ namespace VotingWeb.Controllers
             });
 
             _manager.SaveElectionResults(electionResults);
-
+            log.Info("Sending User to Home Screen");
             return RedirectToAction("Index", "Home");
         }
     }
